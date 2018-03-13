7e8a23f36f356d405bb1be90076993767803adef
Jakub Adamczyk <jakub.adamczyk@imac-11.local>
2018-03-13 10:12:36 +0100

    z laborek

--- IAhp.ts
+++ IAhp.ts
@@ -6,11 +6,45 @@ export interface IAhp {
 
 interface IAlternative {
   name:string;
-  score?:number;
+  final_score?:number;
 }
 
 interface ICriteria {
   name:string;
-  score_sum:number;
-  subcriteria:Array<ICriteria>;
+  weight:number;
+  subcriteria?:Array<ICriteria>;
+  weights?:Array<number>;
+}
+
+
+var ahp = {
+  goal: 'car',
+  alternatives: [
+    {name: 'car1', final_score: null},
+    {name: 'car2', final_score: null},
+    {name: 'car3', final_score: null}
+  ],
+  criteria: [
+    {
+      name: 'engine',
+      weight: 0.45,
+      subcriteria: [
+        {
+          name: 'horsepower',
+          weight: 0.5,
+          weights: [0.3, 0.3, 0.4]
+        },
+        {
+          name: 'durability',
+          weight: 0.5,
+          weights: [0.3, 0.3, 0.4]
+        }
+      ]
+    },
+    {
+      name: 'design',
+      weight: 0.65,
+      weights: [0.4, 0.4, 0.2]
+    }
+  ]
 }
